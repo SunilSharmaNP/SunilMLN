2bde3bf949b728c74d19ee88f3ba59f9f9b314dc
Sunil Sharma <Arnil1718@gmail.com>
2026-05-16 07:05:12 +0545

    Update handlers.py

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -322,6 +322,7 @@ def add_handlers():
     )
     TgClient.bot.add_handler(CallbackQueryHandler(stats_pages, filters=regex("^stats")))
     TgClient.bot.add_handler(CallbackQueryHandler(log_cb, filters=regex("^log")))
+    TgClient.bot.add_handler(CallbackQueryHandler(botpm_callback, filters=regex("^botpm")))
     TgClient.bot.add_handler(CallbackQueryHandler(start_cb, filters=regex("^start")))
     TgClient.bot.add_handler(
         MessageHandler(
