9255b4693306072c99048f7ea3ff8652323ae221
Sunil Sharma <Arnil1718@gmail.com>
2026-05-16 07:23:51 +0545

    Update __init__.py

--- bot/helper/video_utils/__init__.py
+++ bot/helper/video_utils/__init__.py
@@ -1,1 +1,12 @@
-j
+VID_MODE = {
+    'vid_vid':   'Merge Videos',
+    'vid_aud':   'Merge Audio Tracks',
+    'vid_sub':   'Merge Subtitles',
+    'subsync':   'Subtitle Sync',
+    'compress':  'Compress Video',
+    'convert':   'Convert Resolution',
+    'watermark': 'Add Watermark',
+    'extract':   'Extract Stream',
+    'trim':      'Trim Video',
+    'rmstream':  'Remove Stream',
+}
